c261792b1547dfecdf5d798df1fd15bf7384c988
palich <rautaruukkipalich@gmail.com>
2024-06-11 14:01:22 +0500

    edit handlers

--- internal/apiserver/cached.go
+++ internal/apiserver/cached.go
@@ -9,39 +9,46 @@ import (
 	logger "github.com/rautaruukkipalich/urlsh/pkg/log"
 )
 
-func (a *APIServer) GetLongURL(ctx context.Context, urls *model.URLs) (bool, error) {
-	const op = "internal.apiserver.GetLongURL"
+func (a *APIServer) CachedGetLongURL(ctx context.Context, urls *model.URLs) (bool, error) {
+	const op = "internal.apiserver.CachedGetLongURL"
 	ctx = logger.AddTrace(ctx, slog.Any("op", op))
 
 	ok, err := a.cache.get.GetLongURL(ctx, urls)
 	if err != nil {
 		if err != cache.ErrNotFound{
-			return false, err
+			logger.LoggerFromContext(ctx).Info("err get longurl form cache")
+			// return false, err
 		}
 	}
 	if ok {
 		ctx = logger.AddAttr(ctx, "longurl", urls.Long)
 		logger.LoggerFromContext(ctx).Info("longurl form cache")
-		return true, nil
+		return ok, nil
 	}
 
 	ok, err = a.store.get.GetLongURL(ctx, urls)
-	if err != nil || !ok{
+	if err != nil{
+		logger.LoggerFromContext(ctx).Info("err get longurl form store")
 		return false, err
 	}
-
-	if err := a.cache.set.SetURLs(ctx, urls); err != nil {
-		return false, err
+	if !ok{
+		logger.LoggerFromContext(ctx).Info("not fount in store")
+		return ok, nil
 	}
 
 	ctx = logger.AddAttr(ctx, "longurl", urls.Long)
+
+	if err := a.cache.set.SetURLs(ctx, urls); err != nil {
+		logger.LoggerFromContext(ctx).Info("err set urls to cache")
+		return false, err
+	}
 	logger.LoggerFromContext(ctx).Info("cached longurl")
 
 	return true, nil
 }
 
-func (a *APIServer) GetShortURL(ctx context.Context, urls *model.URLs) (bool, error) {
-	const op = "internal.apiserver.GetShortURL"
+func (a *APIServer) CachedGetShortURL(ctx context.Context, urls *model.URLs) (bool, error) {
+	const op = "internal.apiserver.CachedGetShortURL"
 	ctx = logger.AddTrace(ctx, slog.Any("op", op))
 
 	ok, err := a.cache.get.GetShortURL(ctx, urls)
@@ -65,8 +72,8 @@ func (a *APIServer) GetShortURL(ctx context.Context, urls *model.URLs) (bool, er
 	return true, nil
 }
 
-func (a *APIServer) SetURLs(ctx context.Context, urls *model.URLs) (err error) {
-	const op = "internal.apiserver.SetURLs"
+func (a *APIServer) CachedSetURLs(ctx context.Context, urls *model.URLs) (err error) {
+	const op = "internal.apiserver.CachedSetURLs"
 	ctx = logger.AddTrace(ctx, slog.String("op", op))
 
 	if err := a.store.set.SetURLs(ctx, urls); err != nil {
--- internal/apiserver/helper.go
+++ internal/apiserver/helper.go
@@ -2,22 +2,24 @@ package apiserver
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"log/slog"
 	"math/rand"
 	"net/http"
 	"regexp"
 
+	"github.com/rautaruukkipalich/urlsh/internal/model"
 	logger "github.com/rautaruukkipalich/urlsh/pkg/log"
 )
 
 const (
-	Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@%^*+-_"
+	Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@-_"
 	URLlength = 8
 )
 
-func (a *APIServer) error(ctx context.Context, w http.ResponseWriter, r *http.Request, err errorResponse) {
-	a.JSONrespond(ctx, w, r, err.Code, err)
+func (a *APIServer) error(ctx context.Context, w http.ResponseWriter, r *http.Request, code int, err error) {
+	a.JSONrespond(ctx, w, r, code, errorResponse{Code: code, Error: err.Error()})
 }
 
 func (a *APIServer) JSONrespond(ctx context.Context, w http.ResponseWriter, r *http.Request, code int, data any) {
@@ -73,3 +75,67 @@ func (a *APIServer) vaidateUrl(url string) error {
 // 	// ).Info("reditected")
 // 	http.Redirect(w, r, url, http.StatusPermanentRedirect)
 // }
+
+
+func (a *APIServer) GetShortURL(ctx context.Context, urls *model.URLs) (ok bool, err error){
+	const op = "internal.apiserver.GetShortURL"
+	ctx = logger.AddTrace(ctx, slog.Any("op", op))
+
+	ok, err = a.CachedGetLongURL(ctx, urls)
+
+	if !ok && err == nil {
+		logger.LoggerFromContext(ctx).Debug("long url from short url not found")
+		return false, nil
+	}
+	if err != nil {
+		logger.LoggerFromContext(ctx).Debug("error while get cached long url", slog.Any("error", err.Error()))
+		return false, ErrInternalServerError
+	}
+
+	return true, nil
+}
+
+func (a *APIServer) PushURL(ctx context.Context, urls *model.URLs) (int, error){
+	const op = "internal.apiserver.PushURL"
+	ctx = logger.AddTrace(ctx, slog.Any("op", op))
+
+	ok, err := a.CachedGetShortURL(ctx, urls)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			logger.LoggerFromContext(ctx).Debug("err while get short url from cache", slog.String("err", err.Error()))
+			return http.StatusInternalServerError, ErrInternalServerError
+		}
+	}
+
+	if ok {
+		ctx = logger.AddAttr(ctx, "shorturl", urls.Short)
+	}
+
+	if urls.Short == EmptyString {
+		for {
+			short := a.generateShortUrl()
+			urls.Short = short
+
+			ctx = logger.AddAttr(ctx, "shorturl", urls.Short)
+
+			ok, err = a.CachedGetLongURL(ctx, urls)
+			if err != nil {
+				logger.LoggerFromContext(ctx).Debug("err while get long url", slog.String("err", err.Error()))
+				return http.StatusInternalServerError, ErrInternalServerError
+			}
+
+			if ok {
+				continue
+			}
+
+			if err := a.CachedSetURLs(ctx, urls); err != nil {
+				logger.LoggerFromContext(ctx).Debug("error while set urls error", slog.String("err", err.Error()))
+				continue
+			}
+			logger.LoggerFromContext(ctx).Debug("short url generated")
+			break
+		}
+	}
+
+	return http.StatusOK, nil
+}
